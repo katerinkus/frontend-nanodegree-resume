43de9b525f97a273f990cef8440956e3545fbf30
katerinkus <katerinka.07@gmail.com>
2016-11-24 17:06:54 -0800

    Add object work with array jobs

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,40 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work = {
+  "jobs" : [
+    {
+      "employer" : "HiVE",
+      "title" : "Event Animator",
+      "location" : "Vancouver, BC",
+      "dates" : "September 2016-current",
+      "description" : "A part-time gig where I supervise events"
+    },
+    {
+      "employer" : "TetraTech EBA",
+      "title" : "Environmental Technician",
+      "location" : "Vancouver, BC",
+      "dates" : "August 2016-November 2016",
+      "description" : "Researched recycling attittude and current status in CNV"
+    },
+    {
+      "employer" : "Vancouver Photowalks",
+      "title" : "Community Manager & Photo Guide",
+      "location" : "Vancouver, BC",
+      "dates" : "December 2013-May 2016",
+      "description" : "Taught people photography while also guiding them through Vancouver. In the last year, I have also managed social media. Website visits from Facebook went up by 500%. Instagram following grew from 3 hundred to 11 hundred."
+    }
+  ]
+}
+
+
+
+
+
+projects
+projects contains an array of projects. Each object in the projects array should contain title, dates and description strings, and an images array with URL strings for project images.
+
+bio
+bio contains name, role, welcomeMessage, and biopic strings, contacts object and skills array of skill strings. The contacts object should contain a mobile number, email address, github username, twitter handle and location. The twitter property is optional.
+
+education
+education contains an array of schools. Each object in the schools array contains name, location, degree dates and url strings, and amajors array of major strings.
+
+education also contains an onlineCourses array. Each object in the onlineCourses array should contain a title, school, dates and url strings.
